cbd869013636a6ccdd6304736ef038c9c14e4ebd
NOCKTOWL <mehedi.hasanhulknabil@gmail.com>
2025-03-04 21:49:14 +0600

    add home page layout with welcome message and styling

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="font-lg text-white bg-blue-500">
+      <h1>Home</h1>
+      <p>Welcome to the home page</p>
+    </div>
+  );
 }
