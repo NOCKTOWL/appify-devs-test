4e1f0604f1ed5b98ac5535c72ca66cb4974f5fad
NOCKTOWL <mehedi.hasanhulknabil@gmail.com>
2025-03-05 06:28:05 +0600

    enhance chat component with message history and GSAP animations; update sidebar styles and global font

--- app/components/Chat.jsx
+++ app/components/Chat.jsx
@@ -1,17 +1,35 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+
+import { SiRobotframework } from "react-icons/si";
+import { FaRegUser } from "react-icons/fa";
 
 const Chat = () => {
   const [message, setMessage] = useState("");
-  const [response, setResponse] = useState("");
-  const [chatHistory, setChatHistory] = useState([]);
+  const [response, setResponses] = useState("");
+  const [messages, setMessages] = useState([]);
 
   const [loading, setLoading] = useState(false);
 
+  gsap.registerPlugin(TextPlugin);
+
+  const chatRef = useRef(null);
+
+  // gsap.to(chatRef.current, {
+  //   duration: response.length * 0.01,
+  //   delay: 1,
+  //   text: response,
+  //   ease: "none",
+  //   repeat: 0,
+  // });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    localStorage.setItem("message", message);
     try {
       axios
         .post(
@@ -29,7 +47,14 @@ const Chat = () => {
         )
         .then((response) => {
           console.log(response.data);
-          setResponse(response.data.choices[0].message.content);
+          setResponses(response.data.choices[0].message.content);
+          setMessages([
+            ...messages,
+            {
+              user: message,
+              response: response.data.choices[0].message.content,
+            },
+          ]);
         })
         .catch((error) => console.error(error));
     } catch (err) {
@@ -39,12 +64,38 @@ const Chat = () => {
 
   return (
     <div className="h-screen flex flex-col gap-4">
-      <div className="flex-1 bg-gray-400">
-        <div className="px-4 py-2">{response}</div>
+      <div className="flex-1 bg-neutral-900 px-4 py-2 mx-4 mt-4 rounded-xl overflow-y-auto">
+        <div className="px-4 py-2">
+          {messages.map((msg, index) => (
+            <div key={index} className="w-full flex flex-col gap-4">
+              <div className="flex justify-end">
+                <div className="flex items-center max-w-1/2  text-white ">
+                  <div className="px-4 py-2 bg-blue-400 rounded-xl">
+                    {msg.user}
+                  </div>
+                  <div className="bg-neutral-700 rounded-full p-4 ml-4">
+                    <FaRegUser />
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-start">
+                <div
+                  className="flex items-center w-max px-4 py-2 text-white rounded-xl"
+                  ref={chatRef}
+                >
+                  <div className="bg-neutral-700 rounded-full p-4 mr-4">
+                    <SiRobotframework size={20} />
+                  </div>
+                  {msg.response}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <form
         action="submit"
-        className="flex gap-4"
+        className="flex gap-4 p-4"
         onSubmit={(e) => handleSubmit(e)}
       >
         <input
--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
   return (
-    <div className="h-screen border-2 border-blue-300 rounded flex flex-col gap-4">
+    <div className="h-screen rounded flex flex-col gap-4">
       <div className="w-full flex items-center justify-center px-4 py-2 border-2 rounded-xl">
         LOGO
       </div>
